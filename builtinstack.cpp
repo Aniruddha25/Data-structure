80d5a3ba3e4f7d2ea234bdfb0d9df34afcc3591e
ANIRUDDHA Kulkarni <41265081+Aniruddha25@users.noreply.github.com>
2020-04-07 12:17:02 +0530

    Add files via upload

---
+++ builtinstack.cpp
@@ -0,0 +1,29 @@
+//============================================================================
+// Name        : builtinstack.cpp
+// Author      : Aniruddha
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main() {
+	stack<string> s;
+	s.push("ab");
+	s.push("cd");
+	s.push("ef");
+	s.pop();
+	cout<<s.top();
+	cout<<endl;
+	cout<<s.size();
+
+
+
+
+
+}
